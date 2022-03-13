0f2a91817ec21d5e143091dbfe9504b1693632ac
shonmikl <mikhailegorov676@gmail.com>
2022-03-13 19:45:14 +0300

    Initial commit

---
+++ src/main/java/com/mikhailegorov/spring/rest/controller/MyRESTController.java
@@ -0,0 +1,35 @@
+package com.mikhailegorov.spring.rest.controller;
+
+import com.mikhailegorov.spring.rest.entity.Employee;
+import com.mikhailegorov.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRESTController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        return employeeService.getEmployee(id);
+    }
+
+
+
+
+
+
+}
---
+++ src/main/java/com/mikhailegorov/spring/rest/dao/EmployeeDAOImpl.java
@@ -0,0 +1,40 @@
+package com.mikhailegorov.spring.rest.dao;
+
+import com.mikhailegorov.spring.rest.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("from Employee", Employee.class).getResultList();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        Session session = sessionFactory.getCurrentSession();
+        session.saveOrUpdate(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Employee.class, id);
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        session.createQuery("delete from Employee " + "where id =:employeeId").setParameter("employeeId", id).executeUpdate();
+    }
+}
